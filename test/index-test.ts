473eaac5e2924b7a62940d15c427325fd4ed8c0d
tFury <thomas_haenig@gmx.de>
2018-05-30 22:50:03 +0200

    initial set up of project
    Signed-off-by: tFury <thomas_haenig@gmx.de>

---
+++ test/index-test.ts
@@ -0,0 +1,13 @@
+//#region
+import * as chai from "chai";
+import * as chaiAsPromised from "chai-as-promised";
+//#endregion
+describe("pseudoCheck", () => {
+
+    describe("test something", () => {
+        it("should return 2", (done) => {
+            chai.expect(2).to.equal(2);
+            done();
+        });
+    });
+});
