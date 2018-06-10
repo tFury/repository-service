c94a061cbbe95cd1a28b4ec253af231d783ed582
tFury <thomas_haenig@gmx.de>
2018-06-10 21:33:18 +0200

    include table endpoint to get all tables from db, except the system tables
    Signed-off-by: tFury <thomas_haenig@gmx.de>

---
+++ src/lib/postgresProvider.ts
@@ -0,0 +1,85 @@
+//#region import
+import { IField,
+         IDbConnection,
+         ITableFieldDef,
+         ITables }                  from "../lib/interfaces";
+import { Pool,
+         Client,
+         PoolClient }               from "pg";
+import { Logger,
+         ETransportType,
+         ELoglevel }                from "../../node_modules/letslog/src/index";
+import * as errorCollection         from "../lib/error";
+//#endregion
+
+//#region CONST DEFINITION
+const loglevel: string = process.argv[2];
+//#endregion
+
+export class PostgresProvider {
+
+    private pool: Pool;
+    private logger: Logger;
+
+    constructor(config: IDbConnection) {
+
+        this.logger = new Logger({
+            transports: [
+                {
+                    baseComment: "tables.ts",
+                    loglvl: ELoglevel[loglevel],
+                    type: ETransportType.console,
+                    showBaseComment: true,
+                    showDate: true,
+                    showLoglevel: true
+                }
+            ]
+        });
+
+        this.pool = new Pool({
+            host: config.host,
+            user: config.username,
+            password: config.password,
+            database: config.dbName,
+            port: config.port,
+            max: 20,
+            idleTimeoutMillis: 30000,
+            connectionTimeoutMillis: 2000,
+        });
+    }
+
+    /**
+     * getAllTableNames
+     */
+    public getAllTables(): Promise<ITables[]> {
+        this.logger.info("fcn - getAllTableNames");
+        return new Promise((resolve, reject) => {
+
+            let sql: string = `
+                select *
+                from pg_tables
+                where schemaname!='pg_catalog' and schemaname!='information_schema'
+            `;
+
+            let requestClient: PoolClient;
+
+            this.pool.connect()
+            .then((client) => {
+                this.logger.trace("connected");
+                requestClient = client;
+                return client.query(sql);
+            })
+            .then((res) => {
+                this.logger.trace("queried");
+                requestClient.release();
+                resolve(res.rows);
+            })
+            .catch((error) => {
+                this.logger.error("ERROR in getAllTableNames", error);
+                requestClient.release();
+                reject(error);
+            });
+        });
+    }
+
+}
