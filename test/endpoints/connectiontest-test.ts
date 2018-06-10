618cb3fc6b263b31c7b4628d3483c58f50090e61
tFury <thomas_haenig@gmx.de>
2018-06-10 14:40:50 +0200

    include exit flag, to say mocha to close the server after testing
    Signed-off-by: tFury <thomas_haenig@gmx.de>

--- test/endpoints/connectiontest-test.ts
+++ test/endpoints/connectiontest-test.ts
@@ -5,24 +5,23 @@ import { RepositoryService } from "../../src/index";
 
 
 let service = new RepositoryService(require("../config.json")).app;
-let should = chai.should();
-
 chai.use(chaiHttp);
+
 describe("connectiontest", () => {
+
+  const request = chai.request(service);
 
     /*
     * Test the /Connectiontest route
     */
     describe("/v1/connectiontest", () => {
-      it("it should return success", (done) => {
-        chai.request(service)
-            .get("/v1/connectiontest")
+        it("it should return success", (done) => {
+            request.get("/v1/connectiontest")
             .end((err, res) => {
                 chai.expect(res).to.have.status(200);
                 chai.expect(res.body.msg).to.equal("success");
-              done();
+                done();
             });
-      });
-  });
-
+        });
+    });
 });
